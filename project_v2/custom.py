45d284a2068fc1deaf566042692d33c93211c34f
German <gcabrera@evobanco.com>
2021-02-23 18:39:10 +0100

    project_v2 for datarobot custom model test

--- project_v2/custom.py
+++ project_v2/custom.py
@@ -2,7 +2,9 @@ import pandas as pd
 import pickle
 import sys
 
-def transform(input_data, model):
+#def load_model()
+
+def transform(data, model):
     """
     Note: This hook may not have to be implemented for your model.
     In this case implemented for the model used in the example.
@@ -10,21 +12,21 @@ def transform(input_data, model):
     used to implement one-hot encoding for models that don't include it on their own.
     Parameters
     ----------
-    data: pd.DataFrame
-    model: object, the deserialized model
+    data: dataset
     Returns
     -------
     pd.DataFrame
     """
     try:
         #preprocess the input
-        scaler_preprocessor = pickle.load(open(sys.path[0] + "/preprocessor_scaler.pickle", "rb"))
-        input_data = pd.DataFrame(input_data)
+        scaler_preprocessor = pickle.load(open(sys.path[0]+"/preprocessor_artifacts/preprocessor_scaler.pkl", "rb"))
+        input_data = pd.DataFrame(data)
         
-        scaled_input_sample = scaler_preprocessor.transform(input_data.values) 
-        
+        scaled_input_sample = scaler_preprocessor.transform(input_data[input_data.columns[:-1]])
+
         return scaled_input_sample
 
     except Exception as exc:
         print('at transform: ',exc)
-        return 1
+        return exc
+
